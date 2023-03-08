953e9106e7c7c017b8c51449c46ead292f0193fa
Luis Rivas <69378112+Lnrivas@users.noreply.github.com>
2023-03-07 22:19:13 -0600

    improved prompt creation

--- src/Generator.py
+++ src/Generator.py
@@ -1,29 +1,30 @@
-# import openai
-#
-# openai.ChatCompletion.create(
-#   model="gpt-3.5-turbo",
-#   messages=[
-#         {"role": "system", "content": "You are a very kind helpdesk agent."},
-#         {"role": "user", "content": "How can I fix my computer if it is not turning on?"},
-#         {"role": "assistant", "content": "Did you try to turn it off and on again?"},
-#         {"role": "user", "content": "Yes, I did. What can be the problem?"}
-#     ]
-# )
-
-import os
 import openai
-openai.organization = "org-sOZGOhaWYNTrQ6u683TleyYE"
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.Model.list()
+import Group
+
+openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "system", "content": "You are a very kind helpdesk agent."},
+        {"role": "user", "content": "How can I fix my computer if it is not turning on?"},
+        {"role": "assistant", "content": "Did you try to turn it off and on again?"},
+        {"role": "user", "content": "Yes, I did. What can be the problem?"}
+    ]
+)
+
+# import os
+# import openai
+# openai.organization = "org-sOZGOhaWYNTrQ6u683TleyYE"
+# openai.api_key = os.getenv("OPENAI_API_KEY")
+# openai.Model.list()
 
 
 # Class Generator
 class Generator:
     # initializes Member's attributes (name, skills, interests)
-    def __init__(self):
-        self.prompt = ""
+    def __init__(self, Group = Group()):
+        self.prompt = "Crate a list of project ideas. " #improve prompt later
         self.ideas = []
-        self.group = Group()
+        self.group = Group() # FIX USAGE OF GROUP IN THIS CLASS
 
     def getPrompt(self):
         return self.prompt
@@ -31,8 +32,13 @@ class Generator:
     def getIdeas(self):
         return self.ideas
 
-    def createPrompt(self, skills, interests, ):
-        prompt += "" #fix prompt creation later
+    def createPrompt(self, skills, interests):
+        prompt += "Here is a list of skills the project members have: "
+        for skill in skills:
+            prompt += skill
+        prompt += "Here is a list of interests the project members have: "
+        for interest in interests:
+            prompt += interest
 
     def generate(self):
         # send to chatGPT here
