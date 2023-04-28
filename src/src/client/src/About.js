7b2839cf671e09fb417f4e6333f16a54147a9306
Luis Rivas <69378112+Lnrivas@users.noreply.github.com>
2023-04-27 18:11:31 -0700

    uploading a basic 'About' page

--- src/src/client/src/About.js
+++ src/src/client/src/About.js
@@ -3,8 +3,16 @@ import React from "react";
 function About() {
     return (
         <div>
-            <h1 class="text-center"> About Us</h1>
+            <h1 class="text-center"> About</h1>
+            <h3 class="text-center"> BrainstormBoost is a program that uses ai to come up with project ideas for a group of people. All you need to do is enter the members in your group, what their skills and interests are, and describe what kind of project you want. Then it will generate a list of ideas for your group to vote on.</h3>
+            <h2 class="text-center"> Team</h2>
+            <div>
+                <h5 class="text-center">Minna Yu, Luis Rivas, Gilberto Arellano</h5>
+            </div>
         </div>
+
+
+
     );
 }
 export default About;
